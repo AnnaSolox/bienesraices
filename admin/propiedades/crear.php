88c81fe29915373cca8bfcb4b1d4786ca8a8a10e
AnnaSolox <anna.sol.loz@gmail.com>
2025-09-01 10:03:47 +0000

    validacion formulario error en interfaz

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -7,8 +7,23 @@ require '../../includes/funciones.php';
 incluirTemplate('header');
 
 // Array con mensajes de error
-$errores = [];
+$errores = [
+    'titulo' => '',
+    'precio' => '',
+    'descripcion' => '',
+    'habitaciones' => '',
+    'wc' => '',
+    'estacionamiento' => '',
+    'vendedor' => '',
+];
 
+    $titulo = '';
+    $precio = '';
+    $descripcion = '';
+    $habitaciones = '';
+    $wc = '';
+    $estacionamiento = '';
+    $vendedor_id = '';
 
 // Ejecutar el código después de enviar formulario
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
@@ -21,31 +36,31 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $vendedor_id = $_POST['vendedor'] ?? '';
 
     if (!$titulo) {
-        $errores[] = "Debes añadir un título";
+        $errores['titulo'] = "Debes añadir un título";
     }
 
     if (!$precio) {
-        $errores[] = "El precio es obligatorio";
+        $errores['precio'] = "El precio es obligatorio";
     }
 
     if (strlen($descripcion) < 50) {
-        $errores[] = "La descripción es obligatoria y debe tener más de 50 caracteres";
+        $errores['descripcion'] = "La descripción es obligatoria y debe tener más de 50 caracteres";
     }
 
     if (!$habitaciones) {
-        $errores[] = "El número de habitaciones es obligatorio";
+        $errores['habitaciones'] = "El número de habitaciones es obligatorio";
     }
 
     if (!$wc) {
-        $errores[] = "El número de baños es obligatorio";
+        $errores['wc'] = "El número de baños es obligatorio";
     }
 
     if (!$estacionamiento) {
-        $errores[] = "El número de lugares de estacionamiento es obligatorio";
+        $errores['estacionamiento'] = "El número de lugares de estacionamiento es obligatorio";
     }
 
     if (!$vendedor_id) {
-        $errores[] = "Elige un vendedor";
+        $errores['vendedor'] = "Elige un vendedor";
     }
 
     // Revisar que no haya errores
@@ -68,40 +83,52 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     <a href="/admin" class="boton-verde">Volver</a>
 
-    <?php foreach($errores as $error):  ?>
-        <div class="alerta error">
-            <?php echo $error ?>
-        </div>
-    <?php endforeach; ?>
-
     <form class="formulario" method="POST" action="/admin/propiedades/crear.php">
         <fieldset>
             <legend>Información general</legend>
 
             <label for="titulo">Título:</label>
-            <input type="text" id="titulo" name="titulo" placeholder="Título propiedad">
+            <input type="text" id="titulo" name="titulo" placeholder="Título propiedad" value="<?php echo $titulo; ?>">
+            <?php if($errores['titulo']): ?>
+                <div class="alerta"><?php echo $errores['titulo']; ?></div>
+            <?php endif; ?>
 
             <label for="precio">Precio:</label>
-            <input type="number" id="precio" name="precio" placeholder="Precio propiedad">
+            <input type="number" id="precio" name="precio" placeholder="Precio propiedad" value="<?php echo $precio; ?>">
+            <?php if($errores['precio']): ?>
+                <div class="alerta"><?php echo $errores['precio']; ?></div>
+            <?php endif; ?>
 
             <label for="imagen">Imagen:</label>
             <input type="file" id="imagen" accept="imagen/jpeg, image/png">
 
             <label for="descripcion">Descripción:</label>
-            <textarea id="descripcion" name="descripcion"></textarea>
+            <textarea id="descripcion" name="descripcion"><?php if($descripcion !== '') echo $descripcion; ?></textarea>
+            <?php if($errores['descripcion']): ?>
+                <div class="alerta"><?php echo $errores['descripcion']; ?></div>
+            <?php endif; ?>
         </fieldset>
 
         <fieldset>
             <legend>Información propiedad</legend>
 
             <label for="habitaciones">Habitaciones:</label>
-            <input type="number" id="habitaciones" name="habitaciones" placeholder="Ej: 3" min="1" max="9">
+            <input type="number" id="habitaciones" name="habitaciones" placeholder="Ej: 3" min="1" max="9" value="<?php echo $habitaciones; ?>">
+            <?php if($errores['habitaciones']): ?>
+                <div class="alerta"><?php echo $errores['habitaciones']; ?></div>
+            <?php endif; ?>
 
             <label for="wc">Baños:</label>
-            <input type="number" id="wc" name="wc" placeholder="Ej: 3" min="1" max="9">
+            <input type="number" id="wc" name="wc" placeholder="Ej: 3" min="1" max="9" value="<?php echo $wc; ?>">
+            <?php if($errores['wc']): ?>
+                <div class="alerta"><?php echo $errores['wc']; ?></div>
+            <?php endif; ?>
 
             <label for="estacionamiento">Estacionamiento:</label>
-            <input type="number" id="estacionamiento" name="estacionamiento" placeholder="Ej: 3" min="1" max="9">
+            <input type="number" id="estacionamiento" name="estacionamiento" placeholder="Ej: 3" min="1" max="9" value="<?php echo $estacionamiento; ?>">
+            <?php if($errores['estacionamiento']): ?>
+                <div class="alerta"><?php echo $errores['estacionamiento']; ?></div>
+            <?php endif; ?>
         </fieldset>
 
         <fieldset>
@@ -112,6 +139,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 <option value="1">Anna</option>
                 <option value="2">Erika</option>
             </select>
+            <?php if($errores['vendedor']): ?>
+                <div class="alerta"><?php echo $errores['vendedor']; ?></div>
+            <?php endif; ?>
         </fieldset>
 
         <input type="submit" value="Crear propiedad" class="boton-verde">
