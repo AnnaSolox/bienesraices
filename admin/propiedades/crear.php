8a4d52cce90e7cdfb270cc520e35612762fed8a8
AnnaSolox <anna.sol.loz@gmail.com>
2025-09-09 10:22:35 +0000

    subida propiedad desde clase

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -3,6 +3,8 @@
 require '../../includes/app.php';
 
 use App\Propiedad;
+use Intervention\Image\Drivers\Gd\Driver;
+use Intervention\Image\ImageManager as Image;
 
 estaAutenticado();
 
@@ -12,102 +14,49 @@ $db = conectarDB();
 $consulta_vendedores = "SELECT * FROM vendedores";
 $resultado_vendedores = mysqli_query($db, $consulta_vendedores);
 
+$errores = Propiedad::getErrores();
+
 
 incluirTemplate('header');
 
-// Array con mensajes de error
-$errores = [
-    'titulo' => '',
-    'precio' => '',
-    'descripcion' => '',
-    'habitaciones' => '',
-    'wc' => '',
-    'estacionamiento' => '',
-    'vendedor' => '',
-    'imagen' => ''
-];
-    $titulo = '';
-    $precio = '';
-    $descripcion = '';
-    $habitaciones = '';
-    $wc = '';
-    $estacionamiento = '';
-    $vendedor_id = '';
-    $imagen = '';
+$titulo = '';
+$precio = '';
+$descripcion = '';
+$habitaciones = '';
+$wc = '';
+$estacionamiento = '';
+$vendedor_id = '';
+$imagen = '';
 
 // Ejecutar el código después de enviar formulario
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     $propiedad = new Propiedad($_POST);
-    $propiedad->guardar();
-    debuguear($propiedad);
 
-    /* mysqli_real_escape_string se encarga de validar los datos introducidos por el usuario y evitar sentencias maliciosas */
-    $titulo = mysqli_real_escape_string( $db, $_POST['titulo'] );
-    $precio = mysqli_real_escape_string( $db, $_POST['precio'] );
-    $descripcion = mysqli_real_escape_string( $db, $_POST['descripcion'] );
-    $habitaciones = mysqli_real_escape_string( $db, $_POST['habitaciones'] );
-    $wc = mysqli_real_escape_string( $db, $_POST['wc'] );
-    $estacionamiento = mysqli_real_escape_string( $db, $_POST['estacionamiento'] );
-    $vendedor_id = mysqli_real_escape_string( $db, $_POST['vendedor'] ?? '');
-
-    // Asignar files hacia una variable
-    $imagen = $_FILES['imagen'];
-
-    if (!$titulo) {
-        $errores['titulo'] = "Debes añadir un título";
+    // Generar nombre único
+    $nombreImagen = md5(uniqid(rand(), true)) . ".jpg";
+    if($_FILES['imagen']['tmp_name']){
+        $manager = new Image(Driver::class);
+        $imagen = $manager->read($_FILES['imagen']['tmp_name'])->cover(800, 600);
+        $propiedad->setImagen($nombreImagen);
     }
 
-    if (!$precio) {
-        $errores['precio'] = "El precio es obligatorio";
-    }
-
-    if (strlen($descripcion) < 50) {
-        $errores['descripcion'] = "La descripción es obligatoria y debe tener más de 50 caracteres";
-    }
-
-    if (!$habitaciones) {
-        $errores['habitaciones'] = "El número de habitaciones es obligatorio";
-    }
-
-    if (!$wc) {
-        $errores['wc'] = "El número de baños es obligatorio";
-    }
-
-    if (!$estacionamiento) {
-        $errores['estacionamiento'] = "El número de lugares de estacionamiento es obligatorio";
-    }
-
-    if (!$vendedor_id) {
-        $errores['vendedor'] = "Elige un vendedor";
-    }
-
-    // Validar por tamaño
-    $medida = 1000 * 200;
-
-    if (!$imagen['name']) {
-        $errores['imagen'] = "La imagen es obligatoria";
-    }elseif ($imagen['error']) {
-        $errores['imagen'] = "Ha habido un error al subir la imagen";
-    }else if ($imagen['size'] > $medida) {
-        $errores['imagen'] = "La imagen debe tener un tamaño inferior a 200kb";
-    }
+    $errores = $propiedad->validar();
 
     // Revisar que no haya errores
     if (!array_filter($errores)) {
-        
         /* SUBIDA DE ARCHIVOS */
 
         //Crear carpeta
         $carpetaImagenes = '../../imagenes/';
-        if (!is_dir($carpetaImagenes)){mkdir($carpetaImagenes);}
+        if (!is_dir($carpetaImagenes)) {
+            mkdir($carpetaImagenes);
+        }
 
-        // Generar nombre único
-        $nombreImagen = md5( uniqid( rand(), true ) ) . ".jpg";
+        //Guardar la imagen en el servidor
+        $imagen->save($carpetaImagenes . $nombreImagen);
 
-        // Subir la imagen
-        move_uploaded_file($imagen['tmp_name'], $carpetaImagenes . $nombreImagen);
-
+        $resultado = $propiedad->guardar();
         if ($resultado) {
             header('Location: /admin?resultado=1'); //Redireccionar al panel admin
         }
@@ -126,25 +75,25 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
             <label for="titulo">Título:</label>
             <input type="text" id="titulo" name="titulo" placeholder="Título propiedad" value="<?php echo $titulo; ?>">
-            <?php if($errores['titulo']): ?>
+            <?php if ($errores['titulo']): ?>
                 <div class="alerta error"><?php echo $errores['titulo']; ?></div>
             <?php endif; ?>
 
             <label for="precio">Precio:</label>
             <input type="number" id="precio" name="precio" placeholder="Precio propiedad" value="<?php echo $precio; ?>">
-            <?php if($errores['precio']): ?>
+            <?php if ($errores['precio']): ?>
                 <div class="alerta error"><?php echo $errores['precio']; ?></div>
             <?php endif; ?>
 
             <label for="imagen">Imagen:</label>
             <input type="file" id="imagen" accept="image/jpeg, image/png" name="imagen">
-            <?php if($errores['imagen']): ?>
+            <?php if ($errores['imagen']): ?>
                 <div class="alerta error"><?php echo $errores['imagen']; ?></div>
             <?php endif; ?>
 
             <label for="descripcion">Descripción:</label>
-            <textarea id="descripcion" name="descripcion"><?php if($descripcion !== '') echo $descripcion; ?></textarea>
-            <?php if($errores['descripcion']): ?>
+            <textarea id="descripcion" name="descripcion"><?php if ($descripcion !== '') echo $descripcion; ?></textarea>
+            <?php if ($errores['descripcion']): ?>
                 <div class="alerta error"><?php echo $errores['descripcion']; ?></div>
             <?php endif; ?>
         </fieldset>
@@ -154,20 +103,20 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
             <label for="habitaciones">Habitaciones:</label>
             <input type="number" id="habitaciones" name="habitaciones" placeholder="Ej: 3" min="1" max="9" value="<?php echo $habitaciones; ?>">
-            <?php if($errores['habitaciones']): ?>
-                <div class="alerta"><?php echo $errores['habitaciones']; ?></div>
+            <?php if ($errores['habitaciones']): ?>
+                <div class="alerta error"><?php echo $errores['habitaciones']; ?></div>
             <?php endif; ?>
 
             <label for="wc">Baños:</label>
             <input type="number" id="wc" name="wc" placeholder="Ej: 3" min="1" max="9" value="<?php echo $wc; ?>">
-            <?php if($errores['wc']): ?>
-                <div class="alerta"><?php echo $errores['wc']; ?></div>
+            <?php if ($errores['wc']): ?>
+                <div class="alerta error"><?php echo $errores['wc']; ?></div>
             <?php endif; ?>
 
             <label for="estacionamiento">Estacionamiento:</label>
             <input type="number" id="estacionamiento" name="estacionamiento" placeholder="Ej: 3" min="1" max="9" value="<?php echo $estacionamiento; ?>">
-            <?php if($errores['estacionamiento']): ?>
-                <div class="alerta"><?php echo $errores['estacionamiento']; ?></div>
+            <?php if ($errores['estacionamiento']): ?>
+                <div class="alerta error"><?php echo $errores['estacionamiento']; ?></div>
             <?php endif; ?>
         </fieldset>
 
@@ -176,14 +125,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
             <select name="vendedor_id">
                 <option value="" selected disabled>-- Seleccionar vendedor --</option>
-                <?php while($row = mysqli_fetch_assoc($resultado_vendedores) ): ?>
+                <?php while ($row = mysqli_fetch_assoc($resultado_vendedores)): ?>
                     <option <?php echo $vendedor_id === $row['id'] ? 'selected' : ''; ?> value="<?php echo $row['id']; ?>">
                         <?php echo $row['nombre'] . ' ' . $row['apellido']; ?>
                     </option>
                 <?php endwhile; ?>
             </select>
-            <?php if($errores['vendedor']): ?>
-                <div class="alerta"><?php echo $errores['vendedor']; ?></div>
+            <?php if ($errores['vendedor']): ?>
+                <div class="alerta error"><?php echo $errores['vendedor']; ?></div>
             <?php endif; ?>
         </fieldset>
 
--- classes/propiedad.php
+++ classes/propiedad.php
@@ -9,16 +9,28 @@ class Propiedad
     protected static $db;
     protected static $columnasDB = ['id', 'titulo', 'precio', 'imagen', 'descripcion', 'habitaciones', 'wc', 'estacionamiento', 'creado', 'vendedor_id'];
 
-    public ?int $id;
-    public string $titulo;
-    public string $precio;
-    public string $imagen;
-    public string $descripcion;
-    public int $habitaciones;
-    public int $wc;
-    public int $estacionamiento;
-    public ?int $vendedor_id;
-    public string $creado;
+    //Errores
+    protected static $errores = [
+        'titulo' => '',
+        'precio' => '',
+        'descripcion' => '',
+        'habitaciones' => '',
+        'wc' => '',
+        'estacionamiento' => '',
+        'vendedor' => '',
+        'imagen' => ''
+    ];
+
+    public $id;
+    public $titulo;
+    public $precio;
+    public $imagen;
+    public $descripcion;
+    public $habitaciones;
+    public $wc;
+    public $estacionamiento;
+    public $vendedor_id;
+    public $creado;
 
     // Definir la conexión a la BBDD
     public static function setDB($database)
@@ -32,7 +44,7 @@ class Propiedad
         $this->id = $args['id'] ?? null;
         $this->titulo = $args['titulo'] ?? '';
         $this->precio = $args['precio'] ?? '';
-        $this->imagen = $args['imagen'] ?? 'imagen.jpg';
+        $this->imagen = $args['imagen'] ?? '';
         $this->descripcion = $args['descripcion'] ?? '';
         $this->habitaciones = $args['habitaciones'] ?? 0;
         $this->wc = $args['wc'] ?? 0;
@@ -49,32 +61,86 @@ class Propiedad
         // Insertar en BBDD
         $query = " INSERT INTO propiedades (";
         $query .= join(', ', array_keys($atributos));
-        $query .=" ) VALUES ('";
+        $query .= " ) VALUES ('";
         $query .= join("', '", array_values($atributos));
         $query .= "') ";
 
         $resultado = self::$db->query($query);
-        debuguear($resultado);
+        return $resultado;
     }
 
     // Identificar y unir los atributos de la BBDD.
-    public function atributos(){
+    public function atributos()
+    {
         $atributos = [];
-        foreach (self::$columnasDB as $columna){
-            if($columna === 'id') continue;
+        foreach (self::$columnasDB as $columna) {
+            if ($columna === 'id') continue;
             $atributos[$columna] = $this->$columna;
         }
         return $atributos;
     }
 
-    public function sanitizarAtributos(){
+    public function sanitizarAtributos()
+    {
         $atributos = $this->atributos();
         $sanitizado = [];
 
-        foreach($atributos as $key => $value){
-            $sanitizado[$key] = self::$db->escape_string($value);
+        foreach ($atributos as $key => $value) {
+            if ($value === null) {
+                $sanitizado[$key] = '';
+            } else {
+                $sanitizado[$key] = self::$db->escape_string($value);
+            }
         }
 
         return $sanitizado;
+    }
+
+    public static function getErrores()
+    {
+        return self::$errores;
+    }
+
+    public function validar()
+    {
+        if (!$this->titulo) {
+            self::$errores['titulo'] = "Debes añadir un título";
+        }
+
+        if (!$this->precio) {
+            self::$errores['precio'] = "El precio es obligatorio";
+        }
+
+        if (strlen($this->descripcion) < 50) {
+            self::$errores['descripcion'] = "La descripción es obligatoria y debe tener más de 50 caracteres";
+        }
+
+        if (!$this->habitaciones) {
+            self::$errores['habitaciones'] = "El número de habitaciones es obligatorio";
+        }
+
+        if (!$this->wc) {
+            self::$errores['wc'] = "El número de baños es obligatorio";
+        }
+
+        if (!$this->estacionamiento) {
+            self::$errores['estacionamiento'] = "El número de lugares de estacionamiento es obligatorio";
+        }
+
+        if (!$this->vendedor_id) {
+            self::$errores['vendedor'] = "Elige un vendedor";
+        }
+
+        if (!$this->imagen) {
+            self::$errores['imagen'] = "La imagen es obligatoria";
+        } 
+
+        return self::$errores;
+    }
+
+    public function setImagen($imagen){
+        if($imagen){
+            $this->imagen = $imagen;
+        }
     }
 }
