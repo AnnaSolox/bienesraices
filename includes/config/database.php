c98496be8dcbbf32844ff73440953fff2abc94eb
AnnaSolox <132986309+AnnaSolox@users.noreply.github.com>
2025-09-17 08:32:03 +0200

    Update database.php

--- includes/config/database.php
+++ includes/config/database.php
@@ -2,7 +2,7 @@
 
 function conectarDB() : mysqli {
     $db = new mysqli($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASS'], $_ENV['DB_NAME']);
-    $db->set_charset("urf8mb4");
+    $db->set_charset("utf8mb4");
 
     if (!$db) {
         echo "Error: No se pudo conectar a MySQL." . PHP_EOL;
